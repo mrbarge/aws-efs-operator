7cbefafdee9005f3cbda9c918295678579e649b0
johnxo-rh <jjillell@redhat.com>
2022-08-25 13:56:52 +1000

    Removed runtime and moved to crclient

--- pkg/util/predicate_test.go
+++ pkg/util/predicate_test.go
@@ -6,7 +6,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
+	
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
